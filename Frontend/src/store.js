0784297ca18a770af58e7c3d41fae61adecb2be1
Lᴜғғʏ <149653325+Kishor400@users.noreply.github.com>
2024-01-22 07:11:34 -0800

    Delete Frontend directory

--- Frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import tasksReducer from './slices/tasksSlice'
-
-
-export const store = configureStore({
-    reducer: {
-        tasks:tasksReducer
-    }
-})
