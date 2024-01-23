bdfa43dd6ff930725105ef709eefcfb43d4d0e58
kishor400 <kishor404@outlook.in>
2024-01-22 21:07:10 -0800

    Home page Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './App.css';
 import API from "./API";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-            <Route exact path="/" element={<API />}/>
+            <Route exact path="/" element={<Home />}/>
+            <Route exact path="/apihandle" element={<API />}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import LandingImg from "../assets/Landing.png";
+import "../style/Home.css";
+import "../style/Components.css";
+
+function Home() {
+
+  return (
+    <>
+      <div className='landing'>
+        <div className='Header'>
+          <div className='ro'>
+            <p>ENGLISH</p>
+          </div>
+          <div className='lo'>
+            <a href='/'>Home</a>
+            <a href='/learn'>Learn</a>
+            <a href='/practice'>Practice</a>
+            <a href='/info'>Info</a>
+          </div>
+        </div>
+
+        <div className='Body'>
+          <div className='LL'>
+            <p className='Head'>Language Isn't Barrier In Communication</p>
+            <p className='Para'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt, molestias magni nostrum exercitationem quia accusantium minus libero voluptate iure</p>
+            <div className='buttons'>
+              <a href="/learn" className='glow-button' id='home-buts'>Learn</a>
+              <a href="/practice" className='glow-button'>Practice</a>
+            </div>
+          </div>
+          <div className='RR'>
+            <img src={LandingImg}/>
+          </div>
+        </div>
+        <div className='Foot'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, deserunt.</p>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Home
